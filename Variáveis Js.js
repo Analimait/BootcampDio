fbf2f40869bd1f28d1e416a434e931ae5c684820
Ana <152903670+Analimait@users.noreply.github.com>
2023-12-16 19:08:33 -0300

    Add files via upload

---
+++ Variáveis Js.js
@@ -0,0 +1,11 @@
+// nome, idade, numero de telefone, cpf, enderço, recebe pensão
+// string = armazenar texto
+// number = armazenar numeros 
+// boolean = armazenar valores lógicos (verdadeiro, falso) 
+
+let nomePessoa = "Josicreide do Pythinho"
+let idade = 115
+let numeroTelefone = "11-984713388"
+let cpf = "312.122.312-45"
+let endereco = "Rus dos limoeiros, numero 75, bairro da jaguatirica"
+let recebePensao = true
